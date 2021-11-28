03cf28fee4c9916d72bec67ae9f59b3a5dfdc7e9
Mr-Who-001 <page.volk@yandex.ru>
2021-11-28 13:55:04 +0300

    111

--- main.py
+++ main.py
@@ -20,6 +20,46 @@ class MyWidget(QMainWindow):
             self.tw.setItem(i, 4, QTableWidgetItem(str(result[i][4])))
             self.tw.setItem(i, 5, QTableWidgetItem(str(result[i][5])))
             self.tw.setItem(i, 6, QTableWidgetItem(str(result[i][6])))
+        self.pushButton.clicked.connect(self.f)
+
+    def f(self):
+        self.w2 = Example()
+        self.w2.show()
+        self.hide()
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.g)
+
+    def g(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        result = cur.execute('SELECT * FROM coffe WHERE названиесорта = ?', (self.lineEdit_6.text(),)).fetchall()
+        try:
+            if result == []:
+                cur.execute("INSERT INTO coffe VALUES(null, ?, ?, ?, ?, ?, ?);", (self.lineEdit_6.text(),
+                                                                                  self.lineEdit_3.text(),
+                                                                                  self.lineEdit_2.text(),
+                                                                                  self.lineEdit_4.text(),
+                                                                                  self.lineEdit_5.text(),
+                                                                                  self.lineEdit_7.text(),))
+                con.commit()
+            else:
+                cur.execute('''DELETE FROM coffe WHERE названиесорта = ?''', (self.lineEdit_6.text(),))
+                con.commit()
+                cur.execute("INSERT INTO coffe VALUES(null, ?, ?, ?, ?, ?, ?);", (self.lineEdit_6.text(),
+                                                                                  self.lineEdit_3.text(),
+                                                                                  self.lineEdit_2.text(),
+                                                                                  self.lineEdit_4.text(),
+                                                                                  self.lineEdit_5.text(),
+                                                                                  self.lineEdit_7.text(),))
+                con.commit()
+        except BaseException:
+            print(0)
+
 
 
 if __name__ == '__main__':
